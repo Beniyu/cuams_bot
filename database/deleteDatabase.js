142926dd25c8fca7b54d03264c3bb1cc9ee6c38c
Beniyu <Beniyu@users.noreply.github.com>
2022-03-23 12:44:56 +0000

    Added database creation/deletion scripts

---
+++ database/deleteDatabase.js
@@ -0,0 +1,16 @@
+const prompt = require('prompt');
+
+prompt.start();
+prompt.get(['username', 'password'], function(err, result) {
+	var MongoClient = require('mongodb').MongoClient;
+	var url = "mongodb://" + result.username + ":" + result.password + "@localhost:27017/";
+	MongoClient.connect(url, function(err, db) { 
+		var db1 = db.db("cuams_bot_prod");
+		var db2 = db.db("cuams_bot_staging");
+		db1.dropDatabase();
+		db1.removeUser("botuser");
+		db2.dropDatabase();
+		db2.removeUser("botuser", function(err, r) { db.close(); });
+	});
+});
+
