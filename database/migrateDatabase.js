16a4e1aeb2c1f0efb0d6eb385d370583c6c1dde4
Beniyu <Beniyu@users.noreply.github.com>
2022-04-19 15:56:23 +0100

    Separated channel finding code in migration script in attempt of finding issue.

--- database/migrateDatabase.js
+++ database/migrateDatabase.js
@@ -29,7 +29,9 @@ fs.readFile(databaseCredentialsFile, "utf-8", (err1, data) => {
 		const db = client.db(databaseName);
 		await db.collection("channels").update({}, {'$set': {"anonymousSuggestions": false}});
 		let dbPromises = [];
-		for (let currentChannel of (await db.collection("channels").find({})).toArray()) {
+		let channels = await db.collection("channels").find({});
+		let channelArray = channels.toArray();
+		for (let currentChannel of channelArray) {
 			let newChannel = {};
 			Object.assign(newChannel, currentChannel);
 			newChannel["suggestionChannel"] = newChannel["_id"];
